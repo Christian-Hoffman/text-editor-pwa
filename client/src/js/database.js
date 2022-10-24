059d43d70fb8ef5a8f4b474160bddb126190f589
Christian Hoffman <ChristianDHoffman21@gmail.com>
2022-10-24 13:44:15 -0400

    database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,27 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const textEditorDB = await openDB('jate', 1);
+  const tVar = textEditorDB.transaction('jate', readWrite);
+  const sVar = tVar.objectStore('jate');
+  const request = sVar.put({ id: 0, value: content });
+  const result = await request;
+  console.log('Saved to Database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const textEditorDB = await openDB('jate', 1);
+  const tVar = textEditorDB.transaction('jate', 'readonly');
+  const sVar = tVar.objectStore('jate');
+  const request = sVar.get(0);
+  const result = await request;
+  if (typeof result === 'undefined') {
+    console.log('Data not found');
+  } else {
+    console.log('Data retrieved from database', result.value);
+  }
+};
 
 initdb();
